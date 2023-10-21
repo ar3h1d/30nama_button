aaaefdaa9f59de85fc0b17e92a286353dfc35687
ar3h1d <50639936+ar3h1d@users.noreply.github.com>
2023-10-21 18:08:34 +0300

    minor fix

--- Letterboxd_30nama_Link.js
+++ Letterboxd_30nama_Link.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name         Letterboxd 30nama Link
 // @namespace    https://github.com/ar3h1d/letterboxd_30nama_link
-// @version      0.1
+// @version      0.1.1
 // @description  Adds a button to Letterboxd movie pages that links to the 30nama page of the movie.
 // @author       ar3h1d
 // @match        https://letterboxd.com/film/*
 // @grant        none
 // @icon         https://raw.githubusercontent.com/ar3h1d/letterboxd_30nama_link/main/ltb30n.png
-// @license GPL3;https://github.com/ar3h1d/letterboxd_30nama_link/blob/main/LICENSE
-// @copyright    2023, ar3h1d (https://github.com/ar3h1d)
+// @license      GPL3
 // ==/UserScript==
 
 (function() {
