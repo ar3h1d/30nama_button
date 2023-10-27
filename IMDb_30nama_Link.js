c4a96e07161949be478d876a9b3ce7fb9f971b74
ar3h1d <50639936+ar3h1d@users.noreply.github.com>
2023-10-27 19:58:44 +0300

    Addition of the new IMDb 30nama link script

---
+++ IMDb_30nama_Link.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         IMDb 30nama Link
+// @namespace
+// @version      0.1
+// @description  Adds a button to IMDb movie pages that links to the 30nama page of the movie.
+// @author       ar3h1d
+// @match        https://www.imdb.com/title/*
+// @grant        none
+// @icon
+// @license      GPL3
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Extract movie title from the URL
+    let movieID = document.URL.split("/")[4];
+    if (movieID) {
+        // Create the 30nama link
+        const sinamaLink = `https://30nama.com/search?q=${movieID}`;
+
+        // Create the button element
+        const sinamaButton = document.createElement('button');
+        sinamaButton.className = 'ipc-responsive-button ipc-btn--theme-baseAlt ipc-responsive-button--transition-m ipc-btn--on-textPrimary ipc-responsive-button--single-padding';
+        sinamaButton.href = sinamaLink;
+        sinamaButton.textContent = '30nama';
+        sinamaButton.target = '_blank';
+        sinamaButton.style.marginLeft = '10px';
+        sinamaButton.style.marginRight = '15px';
+        sinamaButton.style.border = '1px solid';
+        sinamaButton.style.color = '#d24040';
+
+        // Add an onclick attribute that calls a function to redirect the user
+        sinamaButton.onclick = function() {
+            window.open(sinamaLink, '_blank');
+        };
+
+        // Find the TMDb button
+        const reviewsButton = document.querySelector('button[aria-label="View all topics"]');
+        if (reviewsButton) {
+            // Insert the 30nama button after the All Topics button
+            reviewsButton.parentNode.insertBefore(sinamaButton, reviewsButton.nextSibling);
+        }
+    }
+})();
