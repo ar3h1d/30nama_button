f74b35512bed751b9e40679dc686eb9794ea01e3
ar3h1d <50639936+ar3h1d@users.noreply.github.com>
2023-12-05 19:37:42 +0300

    Support for imdb's mobile version
    also, changing all the instances of 'sinama' to 'cinama' based on their own usage in their repositories.

--- IMDb_30nama_Link.js
+++ IMDb_30nama_Link.js
@@ -5,6 +5,7 @@
 // @description  Adds a button to IMDb movie pages that links to the 30nama page of the movie.
 // @author       ar3h1d
 // @match        https://www.imdb.com/title/*
+// @match        https://m.imdb.com/title/*
 // @grant        none
 // @icon         https://raw.githubusercontent.com/ar3h1d/letterboxd_30nama_link/main/IMDb_30nama_icon.png
 // @license      GPL3
@@ -17,29 +18,29 @@
     let movieID = document.URL.split("/")[4];
     if (movieID) {
         // Create the 30nama link
-        const sinamaLink = `https://30nama.com/search?q=${movieID}`;
+        const cinamaLink = `https://30nama.com/search?q=${movieID}`;
 
         // Create the button element
-        const sinamaButton = document.createElement('button');
-        sinamaButton.className = 'ipc-responsive-button ipc-btn--theme-baseAlt ipc-responsive-button--transition-m ipc-btn--on-textPrimary ipc-responsive-button--single-padding';
-        sinamaButton.href = sinamaLink;
-        sinamaButton.textContent = '30nama';
-        sinamaButton.target = '_blank';
-        sinamaButton.style.marginLeft = '10px';
-        sinamaButton.style.marginRight = '15px';
-        sinamaButton.style.border = '1px solid';
-        sinamaButton.style.color = '#d24040';
+        const cinamaButton = document.createElement('button');
+        cinamaButton.className = 'ipc-responsive-button ipc-btn--theme-baseAlt ipc-responsive-button--transition-m ipc-btn--on-textPrimary ipc-responsive-button--single-padding';
+        cinamaButton.href = cinamaLink;
+        cinamaButton.textContent = '30nama';
+        cinamaButton.target = '_blank';
+        cinamaButton.style.marginLeft = '10px';
+        cinamaButton.style.marginRight = '15px';
+        cinamaButton.style.border = '1px solid';
+        cinamaButton.style.color = '#d24040';
 
         // Add an onclick attribute that calls a function to redirect the user
-        sinamaButton.onclick = function() {
-            window.open(sinamaLink, '_blank');
+        cinamaButton.onclick = function() {
+            window.open(cinamaLink, '_blank');
         };
 
         // Find the TMDb button
         const reviewsButton = document.querySelector('button[aria-label="View all topics"]');
         if (reviewsButton) {
             // Insert the 30nama button after the All Topics button
-            reviewsButton.parentNode.insertBefore(sinamaButton, reviewsButton.nextSibling);
+            reviewsButton.parentNode.insertBefore(cinamaButton, reviewsButton.nextSibling);
         }
     }
 })();
